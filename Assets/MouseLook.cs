4ea5f4d3d3c76f699d7aee9b56ec4e8a28448622
mothra204 <hoainamdth@gmail.com>
2020-09-04 01:57:02 +0700

    Initial commit

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public float mouseSensitivity = 100f; //Độ nhạy của chuột
+	
+	public Transform playerBody;
+
+	float xRotation = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; /*Mouse di chuyển theo hướng X */
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; /*Mouse di chuyển theo hướng Y */
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		playerBody.Rotate(Vector3.up * mouseX);//Body player di chuyển theo trục X
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController controller;
+
+	public float speed = 12f; // Tốc độ di chuyển
+	public float gravity = -9.81f; // Tốc độ rơi
+	public float jumpHeight = 3f; // Nhảy cao 3f
+
+
+	public Transform groundCheck; // Kiểm tra Ground
+	public float groundDistance = 0.4f; // Khoản cách Ground
+	public LayerMask groundMask; // Bề mặt Ground
+
+	Vector3 velocity;
+	bool isGrounded;
+
+	// Update is called once per frame
+	void Update () {
+
+		isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance, groundMask);
+		if(isGrounded && velocity.y <0)
+        {
+			velocity.y = -2f;
+        }
+
+		float z = Input.GetAxis("Horizontal"); //di qua trái và phải
+		float x = Input.GetAxis("Vertical"); // đi thẳng
+
+		Vector3 move = transform.right * z + transform.forward * x; // Đi chuyển
+
+		controller.Move(move * speed * Time.deltaTime); // tốc độ di chuyển
+
+		if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+		velocity.y += gravity * Time.deltaTime;
+
+		controller.Move(velocity * Time.deltaTime);
+	}
+}
